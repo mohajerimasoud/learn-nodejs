4bb5f3cd760d16b807b9b9c2b5cf1f9a66e39ce6
Masoud Mohajeri <masoudmohajeri.wd@gmail.com>
2022-06-22 13:59:47 +0430

    ...

--- index.ts
+++ index.ts
@@ -1,21 +1,28 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
-  const post = await prisma.post.update({
-    where: { id: 1 },
-    data: { published: true },
-  })
-  console.log(post)
+  try {
+    const post = await prisma.post.update({
+      where: { id: 2 },
+      data: { published: false },
+    });
+    // console.log(post);
+  } catch (error) {
+    console.log("error in getting posts : ", error);
+  }
 }
+
 main()
+  .then((res) => {
+    console.log("main then:", res);
+  })
   .catch((e) => {
-    throw e
+    console.log("main catch:", e);
   })
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });
 
-
-	// https://github.com/prisma/prisma-examples/tree/latest/typescript/rest-express
+// https://github.com/prisma/prisma-examples/tree/latest/typescript/rest-express
